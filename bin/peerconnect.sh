0234dbfa0b0a7cceb11b6b4ee5c18176de096ae6
Ethicaldisguise <7862sadikd@gmail.com>
2024-04-02 11:13:25 +0530

    updated .sh setup file

--- bin/peerconnect.sh
+++ bin/peerconnect.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 source ./env/bin/activate.fish
 source ./env/bin/activate
 
@@ -6,14 +8,20 @@ FLAG_FILE="setup_completed.txt"
 if [ ! -f "$FLAG_FILE" ]; then
     echo "Setting up..."
     python3 -m pip install --upgrade pip
+    pip install venv
     python3 -m venv env
     pip install websockets
     pip install requests
     pip install PyQt5
-    pip install tqdm
     echo "Setup completed." > "$FLAG_FILE"
 fi
-cd ..
-python main.py
 
-# chmod +x start_app.sh
+cd ..
+
+if command -v python3 &>/dev/null; then
+    python3 main.py
+elif command -v python &>/dev/null; then
+    python main.py
+else
+    echo "Python not found. Please install Python 3."
+fi
