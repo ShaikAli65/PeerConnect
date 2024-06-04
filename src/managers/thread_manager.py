81c9bb5b981ee9b6957b67f2c676f2e0813f0a35
Ethicaldisguise <7862sadikd@gmail.com>
2024-06-04 13:01:03 +0530

    added text set()

--- src/managers/thread_manager.py
+++ src/managers/thread_manager.py
@@ -2,21 +2,24 @@ from src.core import *
 from concurrent.futures import ThreadPoolExecutor, Executor
 from src.avails.waiters import ThActuator
 
-
 socket_resources = set()
 
 NOMADS = 1
 REQUESTS = 2
 FILES = 3
 DIRECTORIES = 4
+TEXT = 5
 
 
 class _ThreadControl:
+    __slots__ = 'thread_map',
+
     def __init__(self):
         self.thread_map = {
             NOMADS: set(),
             REQUESTS: set(),
             FILES: set(),
+            TEXT: set(),
             DIRECTORIES: set()
         }
 
