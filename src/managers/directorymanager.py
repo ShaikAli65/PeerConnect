5ddad17eb63af8278632d810d466c932abe31abf
Ethicaldisguise <7862sadikd@gmail.com>
2024-06-04 13:07:52 +0530

    improved zipping/unzipping

--- src/managers/directorymanager.py
+++ src/managers/directorymanager.py
@@ -1,67 +1,55 @@
+import os.path
 import pickle
-from multiprocessing import Process
+import tqdm
+import tempfile
+from pathlib import Path
+from multiprocessing import Process, Queue
 from zipfile import ZipFile, ZIP_DEFLATED
 
-import tqdm
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QFileDialog
 
 from src.avails.fileobject import PeerFilePool, make_file_items
 from src.core import *
 from src.avails import useables as use
-from src.avails import remotepeer as remote_peer
 from src.avails.textobject import DataWeaver
-import tempfile
-
-from pathlib import Path
 
 
-def do_handshake(_sock, _content):
+def do_handshake(_sock, controller, _content):
     bind_addr = (const.THIS_IP, use.get_free_port())
     _content['bind_ip'] = bind_addr
     with connect.create_server(bind_addr, family=const.IP_VERSION, backlog=1) as soc:
-        handshake = DataWeaver(header=const.CMD_RECV_DIR, content=_content, _id=bind_addr)
+        handshake = DataWeaver(header=const.CMD_RECV_DIR, content=_content, _id=const.THIS_OBJECT.id)
         handshake.send(_sock)
-        ggm, _ = soc.accept()
-    return ggm
+        use.echo_print("Waiting for connection...")
+        reads,_,_ = select.select([controller, soc], [], [],64)
+        if soc not in reads or controller.to_stop is True:
+            use.echo_print("Terminating directory sending, refused connection", _content)
+            return
+        use.echo_print("connection succeeded", _content)
+        return soc.accept()[0]
 
 
 def directorySender(_data: DataWeaver, recv_sock):
     receiver_obj = peer_list.get_peer(_data.id)
-    file_name = _data.content
-    if not file_name:
-        file_name = open_directory_dialog_window()
+    file_path = _data.content
+    if not file_path:
+        file_path = open_directory_dialog_window()
     zip_path = None
     try:
-        zip_path = process_zipping(receiver_obj, file_name)
+        zip_path = process_zipping(receiver_obj, file_path)
         files = make_file_items([zip_path,])
         _id = receiver_obj.get_file_count()
         controller = ThreadActuator(threading.current_thread())
-        content = {'file_name':file_name, 'file_id':_id}
-        receiver_sock = do_handshake(recv_sock, content)
-        PeerFilePool(paths=files,_id=_id, control_flag=controller).send_files(receiver_sock)
+        receiver_sock = do_handshake(recv_sock, controller, {'file_name':os.path.basename(file_path), 'file_id':_id})
+        if receiver_sock is None:
+            return
+        with receiver_sock:
+            PeerFilePool(file_items=files, _id=_id, control_flag=controller).send_files(receiver_sock)
     finally:
+        print("sender zip path", zip_path)
         if zip_path:
             zip_path.unlink(missing_ok=True)
-
-
-def directoryReceiver(refer: DataWeaver):
-    with connect.Socket(const.IP_VERSION, const.PROTOCOL) as soc:
-        soc.connect(tuple(refer.content['bind_ip']))
-        files = PeerFilePool(_id=refer.content['file_id'])
-        files.recv_files(soc)
-    for file in files:
-        process_unzipping(Path(file.path))
-
-
-def zipDir(zip_name: Union[str, Path], _target):
-    src_path = Path(_target).expanduser().resolve(strict=True)
-    with ZipFile(zip_name, 'w', ZIP_DEFLATED) as zf:
-        progress = tqdm.tqdm(src_path.rglob('*'), desc="Zipping", unit="files")
-        for file in src_path.rglob('*'):
-            zf.write(file, file.relative_to(src_path.parent))
-            progress.update(1)
-        progress.close()
 
 
 def process_zipping(receiver_obj, target):
@@ -76,6 +64,37 @@ def process_zipping(receiver_obj, target):
     return temp_path
 
 
+def zipDir(zip_name: Union[str, Path], _target):
+    src_path = Path(_target).expanduser().resolve(strict=True)
+    with ZipFile(zip_name, 'w', ZIP_DEFLATED) as zf:
+        progress = tqdm.tqdm(src_path.rglob('*'), desc="Zipping", unit="files")
+        for file in src_path.rglob('*'):
+            zf.write(file, file.relative_to(src_path.parent))
+            progress.update(1)
+        progress.close()
+
+
+def directoryReceiver(refer: DataWeaver):
+    with connect.create_connection(tuple(refer.content['bind_ip'])) as soc:
+        files = PeerFilePool(_id=refer.content['file_id'])
+        files.recv_files(soc)
+    for file in files:
+        process_unzipping(Path(file.path))
+
+
+def process_unzipping(file_path):
+    queue = Queue(1)
+    unzip_process = Process(target=unZipper, args=(file_path, const.PATH_DOWNLOAD, queue))
+    unzip_process.start()
+    # unzip_process.join()
+    file_name = queue.get()
+    unzip_process.join()
+    unzip_process.close()
+    print("file_name:", file_name, file_path)
+
+    file_path.unlink()
+
+
 def unZipper(zip_path, destination_path, name_queue):
     with ZipFile(zip_path, 'r') as zip_ref:
         try:
@@ -83,19 +102,8 @@ def unZipper(zip_path, destination_path, name_queue):
         except PermissionError as pe:
             error_log(f"::PermissionError in unzipper()/directory manager.py :{pe}")
     print(f"::Extracted {zip_path} to {destination_path}")
-    name_queue.put(zip_ref.namelist())
-
-
-def process_unzipping(file_path):
-    import multiprocessing.queues
-    queue = multiprocessing.Queue(1)
-
-    unzip_process = Process(target=unZipper, args=(file_path, const.PATH_DOWNLOAD, queue))
-    unzip_process.start()
-    unzip_process.join()
-    unzip_process.close()
-    file_path.unlink(missing_ok=True)
-    return queue.get()
+    name_queue.put(zip_ref.filename)
+    return
 
 
 def open_directory_dialog_window(_prev_dir=[os.path.join(os.path.expanduser('~'), 'Downloads'), ]):
@@ -103,8 +111,7 @@ def open_directory_dialog_window(_prev_dir=[os.path.join(os.path.expanduser('~')
     dialog = QFileDialog()
     dialog.setOption(QFileDialog.DontUseNativeDialog, True)
     dialog.setWindowFlags(Qt.WindowStaysOnTopHint | dialog.windowFlags())
-    directory = dialog.getExistingDirectory(directory=_prev_dir[0],
-                                            caption="Select directory to send")
+    directory = dialog.getExistingDirectory(directory=_prev_dir[0], caption="Select directory to send")
     _prev_dir[0] = directory
     return directory
 
@@ -138,13 +145,11 @@ def send_files(dir_socket, dir_path):
     for x in dir_path.glob('**/*'):
         if x.is_file():
             pass
-    pass
 
 
 @NotInUse
-def directory_sender(receiver_obj: remote_peer.RemotePeer, dir_path: str):
+def directory_sender(receiver_obj, dir_path: str):
     dir_socket = use.create_conn_to_peer(receiver_obj)
-    # SimplePeerText(dir_socket, const.CMD_RECV_DIR_LITE, byte_able=False).send()
     dir_path = Path(dir_path)
     DataWeaver(header=const.CMD_RECV_DIR, content=dir_path.name, _id=const.THIS_OBJECT.id)
     print(f"sending directory{dir_path} to ", receiver_obj)
@@ -158,7 +163,6 @@ def directory_sender(receiver_obj: remote_peer.RemotePeer, dir_path: str):
 
 @NotInUse
 def directory_receiver(_conn):
-    # use.get_peer_from_id()
     sender_obj = peer_list.get_peer(_conn.getpeername()[0])
     sender_obj.increment_file_count()
     dir_len = struct.unpack('!Q', _conn.recv(8))[0]
