53b0a26543099068c79dd3159797f7a7b3ad8c58
Ethicaldisguise <7862sadikd@gmail.com>
2024-06-04 13:06:13 +0530

    changed thread_per connection to a socket loop

--- src/core/nomad.py
+++ src/core/nomad.py
@@ -22,7 +22,7 @@ class Nomad(object):
         'currently_in_connection':defaultdict,
         'RecentConnections':Any,
     }
-    __slots__ = 'address', 'controller', 'selector', 'main_socket', 'currently_in_connection', 'RecentConnections'
+    __slots__ = 'address', 'controller', 'selector', 'main_socket', 'currently_in_connection', 'RecentConnections', 'socket_handler'
 
     def __init__(self, ip='localhost', port=8088):
         self.address = (ip, port)
@@ -34,6 +34,7 @@ class Nomad(object):
         self.main_socket.bind(self.address)
         self.currently_in_connection = defaultdict(int)
         self.RecentConnections = getattr(import_module('src.core.senders'), 'RecentConnections')
+        self.socket_handler = SocketLoop()
 
     def __resetSocket(self):
         self.main_socket = connect.Socket(const.IP_VERSION, const.PROTOCOL)
@@ -55,89 +56,43 @@ class Nomad(object):
             if self.currently_in_connection[_id] > 5:
                 return None
             self.currently_in_connection[_id] += 1
+            use.echo_print("verified peer", _id)  # debug
             return _id
 
-    def add_receive_thread(self, initial_conn, peer_id):
-        th_controller = ThreadActuator(None, NOMAD_FLAG)
-        thread = use.start_thread(self.connectNew, _args=(initial_conn,peer_id))  # name these threads .??
-        th_controller.thread = thread
-        thread_handler.register(th_controller, NOMADS)
-        self.RecentConnections.addSocket(peer_id, initial_conn)
-
-    def __acceptConnections(self):
+    def __accept_connection(self):
         initial_conn = None
         try:
             initial_conn, _ = self.main_socket.accept()
             use.echo_print(f"New connection from {_}")
             peer_id = self.verify(initial_conn)
             if peer_id:
-                self.add_receive_thread(initial_conn, peer_id)
+                self.socket_handler.register_sock(initial_conn, peer_id)
+                self.RecentConnections.addSocket(peer_id, initial_conn)
             else:
                 initial_conn.close()
         except (socket.error, OSError) as e:
-            error_log(f"Socket error: at {func_str(Nomad.__acceptConnections)} exp:{e}")
+            error_log(f"Socket error: at {func_str(Nomad.__accept_connection)} exp:{e}")
             initial_conn.close()
 
     def initiate(self):
         self.main_socket.listen(3)
         const.PAGE_HANDLE_CALL.wait()
         use.echo_print("::Listening for connections", self.main_socket)
-        while True:
-            reads, _, _ = select.select([self.main_socket, self.controller.reader], [], [])
-            if self.controller.to_stop is True:
-                use.echo_print("::Nomad Object Ended")
-                return
-            if self.main_socket not in reads:
-                continue
-            if len(self.currently_in_connection) > 10:
-                time.sleep(10)  # limiting rate just in case of more no. of different connections
-            self.__acceptConnections()
-
-    def connectNew(self, _conn, peer_id):
-        with _conn:
+        threading.Thread(target=self.socket_handler.start_loop).start()
+        with self.main_socket:
+            reads_list = [self.main_socket, self.controller.reader]
             while True:
-                readable, _, _ = select.select([_conn, self.controller.reader], [], [], 500)
+                reads, _, _ = select.select(reads_list, [], [])
                 if self.controller.to_stop is True:
+                    use.echo_print("::Nomad Object Ended")
                     return
-                if _conn not in readable:
-                    use.echo_print(f"::Connection timed out : at {func_str(self.connectNew)}", _conn.getpeername())
-                    self.disconnectUser(_conn, self.controller, peer_id)
-                    return
-                try:
-                    if _conn.recv(1, socket.MSG_PEEK) == b'':
-                        use.echo_print(f"::Connection closed by peer at {func_str(self.connectNew)}", _conn.getpeername())
-                        return
-                    _data = DataWeaver().receive(_conn)
-                except socket.error as e:
-                    use.echo_print(
-                        f"::Connection error: at {func_str(self.connectNew)} exp:{e}",
-                        _conn.getpeername())
-                    return
-                use.echo_print('data from peer :\n', _data)
-
-                if _data.header == const.CMD_TEXT:
-                    asyncio.run(handle_data.feed_user_data_to_page(_data.content, _data.id))
-                elif _data.header == const.CMD_RECV_FILE:
-                    use.start_thread(_target=filemanager.file_receiver, _args=(_data,))
-                elif _data.header == const.CMD_RECV_FILE_AGAIN:
-                    use.start_thread(_target=filemanager.re_receive_file, _args=(_data,))
-                elif _data.header == const.CMD_RECV_DIR:
-                    directorymanager.directoryReceiver(refer=_data)
-                elif _data.header == const.CMD_CLOSING_HEADER:
-                    self.disconnectUser(_conn, self.controller, peer_id)
-                    return
-
-    def disconnectUser(self, _conn, _controller, _id):
-        self.currently_in_connection[_id] -= 1
-        with _conn:
-            DataWeaver(header=const.CMD_CLOSING_HEADER).send(_conn)
-            use.echo_print(f"::Closing connection at {func_str(Nomad.disconnectUser)}", _conn.getpeername())
-        self.RecentConnections.force_remove(peer_id=_id)
-        _controller.stop()
-        thread_handler.delete(_controller, NOMADS)
+                if len(self.currently_in_connection) > 10:
+                    time.sleep(10)  # limiting rate just in case of more no. of different connections
+                self.__accept_connection()
 
     def end(self):
-        self.controller()
+        self.socket_handler.stop_loop()
+        self.controller.signal_stopping()
         self.currently_in_connection.fromkeys(self.currently_in_connection, 0)
         self.main_socket.close() if self.main_socket else None
 
@@ -146,6 +101,80 @@ class Nomad(object):
 
     def __del__(self):
         self.main_socket.close()
-        self.controller()
-        # for sock in self.currently_in_connection.keys():
-        # sock.close()
+        self.controller.signal_stopping()
+        self.socket_handler.stop_loop()
+
+
+class SocketLoop(selectors.DefaultSelector):
+    # TODO: ADD SOCKET EXPIRY TIMER THREAD
+    def __init__(self):
+        super().__init__()
+        # a custom object to be used to wake select call when needed, instead of polling
+        self._controller = ThreadActuator(threading.current_thread())
+        thread_handler.register(self._controller, NOMADS)
+
+        self.register(self._controller, selectors.EVENT_READ)
+        self.currently_in_connection = defaultdict(int)
+        self.RecentConnections = getattr(import_module('src.core.senders'), 'RecentConnections')
+
+    def register_sock(self, file_desc, peer_id):
+        self.register(file_desc, selectors.EVENT_READ, peer_id)
+        self._controller.write()
+        self._controller.clear_reader()
+        use.echo_print(" ".join(str(x) for x in self.get_map()))
+
+    def start_loop(self):
+        controller = self._controller
+        controller_fileno = controller.fileno()
+        # _selector =
+        while True:
+            events = self.select()
+            if controller.to_stop:
+                use.echo_print("::Nomad connections ended")
+                return
+            use.echo_print("selelected", ' '.join(str(x) for x in events))
+            try:
+                for event, _ in events:
+                    file_desc = event.fileobj
+                    if controller_fileno != file_desc.fileno():
+                        self.connect_new(file_desc, event.data)
+            except (ConnectionResetError,ConnectionAbortedError):
+                pass
+
+    def connect_new(self,_conn, peer_id):
+        try:
+            if _conn.recv(1, socket.MSG_PEEK) == b'':
+                self.unregister(_conn)
+                use.echo_print(f"::Connection closed by peer at {func_str(SocketLoop.connect_new)}", _conn.getpeername())
+                return
+            _data = DataWeaver().receive(_conn)
+            # use.echo_print('data from peer :\n', _data)  # debug
+        except socket.error as e:
+            self.unregister(_conn)
+            use.echo_print(f"::Connection error: at {func_str(SocketLoop.connect_new)} exp:{e}", _conn.getpeername())
+            return
+
+        if _data.header == const.CMD_TEXT:
+            asyncio.run(handle_data.feed_user_data_to_page(_data.content, _data.id))
+        elif _data.header == const.CMD_RECV_FILE:
+            use.start_thread(_target=filemanager.file_receiver, _args=(_data,))
+        elif _data.header == const.CMD_RECV_FILE_AGAIN:
+            use.start_thread(_target=filemanager.re_receive_file, _args=(_data,))
+        elif _data.header == const.CMD_RECV_DIR:
+            use.start_thread(_target=directorymanager.directoryReceiver, _args=(_data,))
+        elif _data.header == const.CMD_CLOSING_HEADER:
+            self.unregister(_conn)
+            self.disconnect_user(_conn, self._controller, peer_id)
+            return
+
+    def disconnect_user(self, _conn, _controller, _id):
+        self.currently_in_connection[_id] -= 1
+        with _conn:
+            if use.is_socket_connected(_conn):
+                DataWeaver(header=const.CMD_CLOSING_HEADER).send(_conn)
+        use.echo_print(f"::Closing connection at {func_str(SocketLoop.disconnect_user)}\n", peer_list.get_peer(_id))
+        self.RecentConnections.force_remove(peer_id=_id)
+        thread_handler.delete(_controller, NOMADS)
+
+    def stop_loop(self):
+        self._controller.signal_stopping()
