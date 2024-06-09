e07c68a6d6e14ceac06a8d7c7e96ff596cc916b1
Ethicaldisguise <7862sadikd@gmail.com>
2024-06-09 19:47:33 +0530

    added end functions of directory_manager and thread_manager

--- src/managers/endmanager.py
+++ src/managers/endmanager.py
@@ -2,7 +2,7 @@ from src.avails import constants as const
 
 from src.core import connectserver as connect_server, requests_handler, senders
 from src.webpage_handlers import handle_data, handle_signals, httphandler
-from src.managers import filemanager
+from src.managers import filemanager, directorymanager, thread_manager
 from src.avails import textobject, remotepeer
 
 
@@ -19,6 +19,7 @@ def end_session(sig='',frame=''):
     connect_server.end_connection_with_server()
     requests_handler.end_requests_connection()
     senders.RecentConnections.end()
+
     # if not const.PAGE_HANDLE_CALL.is_set():
     #     return None
     if const.HOST_OBJ:
@@ -26,7 +27,9 @@ def end_session(sig='',frame=''):
     handle_data.end()
     handle_signals.end()
     # httphandler.end_serving()
+    directorymanager.end_zipping_processes()
     filemanager.endFileThreads()
     textobject.stop_all_text()
     remotepeer.end()
+    thread_manager.thread_handler.signal_stopping()
     exit(1)
