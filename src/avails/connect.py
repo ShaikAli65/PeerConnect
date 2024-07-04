5ab7909636088fd1777e96662bc21945ed29d394
Ethicaldisguise <7862sadikd@gmail.com>
2024-07-04 16:46:17 +0530

    changed kwargs in getaddrinfo()

--- src/avails/connect.py
+++ src/avails/connect.py
@@ -13,30 +13,32 @@ class Socket(socket.socket):
     Just an afterthought for any changes in the way we work with socket
     Like connecting to a peer may be changed in future
     """
-
     def accept(self):
+
         fd, addr = self._accept()
-        # Cast the new socket to the custom Socket class
-        custom_socket = self.__class__(self.family,self.type,self.proto,fileno=fd)
+        # Cast the socket to the custom Socket class
+
+        custom_socket = self.__class__(self.family, self.type, self.proto, fileno=fd)
         if socket.getdefaulttimeout() is None and self.gettimeout():
             custom_socket.setblocking(True)
         return custom_socket, addr
 
 
-def wrap_sock(sock):
-    custom_sock = Socket(sock.family,sock.type,sock.proto,fileno=sock.fileno())
-    # sock.detach()
-    # sock.close()
-    return custom_sock
-
-
-def create_connection(address, timeout=socket.getdefaulttimeout(), source_address=None) -> Socket:
-    # return wrap_sock(socket.create_connection(address, timeout, source_address))
-    address_info = socket.getaddrinfo(address[0], address[1], proto=const.PROTOCOL)[0]
+def create_connection(address, timeout=socket.getdefaulttimeout(), source_address=None, *args) -> Socket:
+    """
+    :param address:
+    :param timeout:
+    :param source_address:
+    :param args: passed into :func:`setsockopt()` mostly used for the case of
+                 setting options before connection is initiated
+    :return sock: connected sock
+    """
+    address_info = socket.getaddrinfo(address[0], address[1], type=const.PROTOCOL)[0]
     address = address_info[4][:2]
     sock_family = address_info[0]
     sock_type = address_info[1]
     sock = Socket(family=sock_family,type=sock_type)
+    sock.setsockopt(*args)
     if source_address:
         sock.bind(source_address)
     sock.settimeout(timeout)
@@ -46,7 +48,7 @@ def create_connection(address, timeout=socket.getdefaulttimeout(), source_addres
 
 def create_server(address,*, family=-1, backlog=-1, dual_stack=False) -> Socket:
     # sock = socket.create_server(address, family=family, backlog=backlog, reuse_port=reuse_port, dualstack_ipv6=dual_stack)
-    address_info = socket.getaddrinfo(address[0], address[1], family=family, proto=const.PROTOCOL)[0]
+    address_info = socket.getaddrinfo(address[0], address[1], family=family, type=const.PROTOCOL)[0]
     address = address_info[4][:2]
     sock_family = address_info[0]
     sock_type = address_info[1]
@@ -62,21 +64,23 @@ BASIC_URI_CONNECT = 13
 REQ_URI_CONNECT = 12
 
 
-def connect_to_peer(_peer_obj=None, peer_id=None, to_which: int = BASIC_URI_CONNECT, timeout=None):
+def connect_to_peer(_peer_obj=None, peer_id=None, to_which: int = BASIC_URI_CONNECT, timeout=None, *args):
     """
     Creates a basic socket connection to peer id passed in, or to the peer_obj passed in.
     peer_obj is given higher priority
     The another argument :param to_which: specifies to what uri should the connection made,
-    pass :param const.REQ_URI_CONNECT: to connect to req_uri of peer
+    pass `const.REQ_URI_CONNECT` to connect to req_uri of peer
+    *args will be passed into socket.setsockoption
     :param timeout:
-    :param _peer_obj:
-    :param peer_id:
+    :param _peer_obj: RemotePeer object
+    :param peer_id: peer's id to connect `with`
     :param to_which:
+    :param args: passed into :func:`create_connection` see it's doc for more info.
     :return:
     """
     peer_obj = _peer_obj or const.LIST_OF_PEERS.get_peer(peer_id)
     addr = peer_obj.req_uri if to_which == REQ_URI_CONNECT else peer_obj.uri
-    return create_connection(addr,timeout)
+    return create_connection(addr,timeout, *args)
 
 
 def read_sock(sock, actuator, data_len, timeout=10) -> Optional[bytes]:
@@ -105,15 +109,14 @@ def is_socket_connected(sock:Socket):
 
 def get_free_port() -> int:
     """Gets a free port from the system."""
-    random_port = random.randint(1024, 65535)
+    random_port = random.randint(10000, 65535)
     while not is_port_empty(random_port):
-        random_port = random.randint(1024, 65535)
+        random_port = random.randint(10000, 65535)
     return random_port
 
 
 def is_port_empty(port):
     try:
-        # print(THIS_IP,IP_VERSION)
         with soc.socket(const.IP_VERSION, soc.SOCK_STREAM) as s:
             s.bind((const.THIS_IP, port))
             return True
