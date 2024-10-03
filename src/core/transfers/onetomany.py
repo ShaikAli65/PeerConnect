0b8bceb2f6dc9aa1a1bb3dd1576e79113b5ce308
Ethicaldisguise <7862sadikd@gmail.com>
2024-10-03 22:32:47 +0530

    init

--- src/core/transfers/onetomany.py
+++ src/core/transfers/onetomany.py
@@ -1,75 +1,17 @@
 import asyncio
-import itertools
-import math
-from collections import defaultdict
 from dataclasses import dataclass
 
 from src.avails import RemotePeer, WireData, const, unpack_datagram
-from . import HEADERS
-from ..connections import Connector
 from .. import get_this_remote_peer
-
-
-def next_power_of_two(n):
-    if n < 1:
-        return 1
-    return 2 ** math.ceil(math.log2(n))
 
 
 class SendOTMBytes:
     def __init__(self, data: bytes, peers: list[RemotePeer], session_id):
         self.data = data
-        self.peer_list = peers.append(get_this_remote_peer())
-        self.adjacency_list = defaultdict(list)
-        self.max_no_peers = next_power_of_two(len(peers))
-        self.dimensions = self.max_no_peers.bit_length() - 1
+        peers.append(get_this_remote_peer())
+        self.peer_list = peers
         self.session_id = session_id
         self.confirmed_peers = None
-
-    async def start(self):
-        """Start the process of sending data to peers."""
-        self.create_hypercube()
-        await self.inform_peers()
-        await self.trigger_spanning_formation()
-
-    def create_hypercube(self):
-        """Create the hypercube topology."""
-        peer_id_to_peer_mapping = {i: peer for i, peer in zip(range(len(self.peer_list)), self.peer_list)}
-        for i in range(len(self.peer_list)):
-            for j in range(self.dimensions):
-                neighbor = i ^ (1 << j)
-                if neighbor < len(self.peer_list):
-                    peer = peer_id_to_peer_mapping[j]
-                    neigh = peer_id_to_peer_mapping[neighbor]
-                    self.adjacency_list[peer.id].append(neigh.id)
-
-    async def inform_peers(self):
-        tasks = [self.contact_peer(peer) for peer in self.peer_list]
-        requested_peers = await asyncio.gather(*tasks, return_exceptions=False)
-        self.confirmed_peers = list(itertools.takewhile(lambda x:x[1], requested_peers))
-        # send an audit event to page confirming peers
-
-    async def contact_peer(self, peer):
-        peer_connection = await Connector.connect_peer(peer)
-        await peer_connection.asendall(self.prepare_request(peer))
-        return peer, True
-
-    def prepare_request(self, peer):
-        return WireData(
-            header=HEADERS.CMD_RECV_FILE_AND_FORWARD,
-            _id=get_this_remote_peer().id,
-            session_id=self.session_id,
-            session_token=self.session_token,
-            adjacent_peers=self.adjacency_list[peer.id],
-        ).__bytes__()
-
-    async def trigger_spanning_formation(self):
-
-        pass
-
-    @property
-    def session_token(self):
-        return "test"  # :todo perform a cipher algorithm
 
 
 @dataclass
---
+++ src/managers/gossip_manager.py
@@ -0,0 +1,83 @@
+import asyncio
+
+from src.avails import PalmTreeResponse, Wire, WireData, connect, const
+from src.core import get_this_remote_peer
+from src.core.transfers import PalmTreeMediator, PalmTreeSession
+
+
+class GossipSessionRegistry:
+    current_sessions = {}
+    completed_session = []
+
+    @classmethod
+    def add_session(cls, mediator):
+        cls.current_sessions[mediator.session.id] = mediator
+
+    @classmethod
+    def get_session(cls, session_id) -> PalmTreeMediator:
+        return cls.current_sessions.get(session_id, None)
+
+    @classmethod
+    def remove_session(cls, session_id):
+        del cls.current_sessions[session_id]
+
+
+def schedule_gossip_session(session: PalmTreeSession, passive_sock, active_endpoint_addr):
+    session_mediator = PalmTreeMediator(session, passive_sock, active_endpoint_addr)
+    session_mediator.start_session()
+    GossipSessionRegistry.add_session(mediator=session_mediator)
+
+
+async def new_gossip_request_arrived(req_data: WireData, addr):
+    loop = asyncio.get_event_loop()
+    connection = await connect.UDPProtocol.create_connection_async(loop, addr)
+    stream_endpoint_addr = get_active_endpoint()
+    datagram_endpoint, datagram_endpoint_addr = get_passive_endpoint(addr, loop)
+    session = PalmTreeSession(
+        originater_id=req_data.id,
+        adjacent_peers=req_data['adjacent_peers'],
+        id=req_data['session_id'],
+        key=req_data['session_key'],
+        max_forwards=req_data['max_forwards']
+    )
+    response = PalmTreeResponse(
+        peer_id=get_this_remote_peer().id,
+        active_addr=stream_endpoint_addr,
+        passive_addr=datagram_endpoint_addr,
+        session_key=req_data['session_key']
+    )
+    schedule_gossip_session(session, datagram_endpoint, stream_endpoint_addr)
+    Wire.send_datagram(connection, addr, bytes(response))
+
+
+def get_passive_endpoint(addr, loop):
+    datagram_endpoint_addr = (get_this_remote_peer().ip, connect.get_free_port())
+    datagram_endpoint = connect.UDPProtocol.create_async_server_sock(
+        loop,
+        addr,
+        family=const.IP_VERSION,
+        backlog=3
+    )
+    return datagram_endpoint, datagram_endpoint_addr
+
+
+def get_active_endpoint():
+    return get_this_remote_peer().uri
+
+
+def get_active_endpoint_socket1():
+    loop = asyncio.get_event_loop()
+    stream_endpoint_addr = (get_this_remote_peer().ip, connect.get_free_port())
+    stream_endpoint = connect.TCPProtocol.create_async_server_sock(
+        loop,
+        stream_endpoint_addr,
+        family=const.IP_VERSION,
+        backlog=3
+    )
+    return stream_endpoint, stream_endpoint_addr
+
+
+def update_gossip_stream_socket(connection_sock: connect.Socket, data: WireData):
+    session_id = data['session_id']
+    mediator = GossipSessionRegistry.get_session(session_id)
+    mediator.add_stream_link(data, connection_sock)
