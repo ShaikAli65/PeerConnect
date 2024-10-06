c4fe95c136d3ab5a398e8c9fd63f27454472bad5
Ethicaldisguise <7862sadikd@gmail.com>
2024-10-06 23:20:25 +0530

    cleaning up protocol suite

--- src/managers/filemanager.py
+++ src/managers/filemanager.py
@@ -2,34 +2,38 @@ import asyncio
 import enum
 import os
 import struct
+from itertools import count
 from pathlib import Path
 
-from src.avails import (
-    dialogs,
-    FileDict,
-    const,
-    connect,
-    WireData,
-    use, RemotePeer,
-)
-from src.core import Dock, connections, get_this_remote_peer
-from src.core import transfers
-
+from src.avails import (FileDict, Wire, WireData, connect, const, dialogs, use)
+from src.core import Dock, connections, get_this_remote_peer, transfers
 from . import processmanager
-
-all_files = FileDict()
+from ..avails.useables import awaitable
+from ..core.transfers import HEADERS, PeerFilePool
 
 
 async def open_file_selector():
     loop = asyncio.get_running_loop()
-    result = loop.run_in_executor(None, dialogs.Dialog.open_file_dialog_window)
-    await result
-
-    return result.result()
+    result = await loop.run_in_executor(None, dialogs.Dialog.open_file_dialog_window)
+    return result
 
 
-def get_id_for_file(peer_obj: RemotePeer):
-    return peer_obj.get_file_id()
+class FileRegistry:
+    all_files = FileDict()
+    file_counter = count()
+
+    @classmethod
+    def get_id_for_file(cls):
+        return next(cls.file_counter)
+
+    @classmethod
+    def get_scheduled_transfer(cls, request_packet):
+        pass
+
+
+def connection_arrived(connection: connect.Socket, data_packet: WireData):
+
+    ...
 
 
 class STATE(enum.Enum):
@@ -48,64 +52,42 @@ class FileSender:
         self.state = STATE.PREPARING
         self.file_list = [transfers.FileItem(os.path.getsize(x), x, seeked=0) for x in file_list]
         self.file_handle = None
+        self.file_id = FileRegistry.get_id_for_file()
+        self.file_pool = None
 
     async def send_files(self, peer_id):
-
         peer_obj = Dock.peer_list.get_peer(peer_id)
-        file_id = get_id_for_file(peer_obj)
-        server_sock, addr = self._prepare_socket()
-        tell_header = self._prepare_header(addr, file_id)
-
+        conn_sock = await connections.Connector.connect_peer(peer_obj)
+        tell_header = WireData(
+            header=HEADERS.CMD_RECV_FILE,
+            _id=get_this_remote_peer().id,
+            file_id=self.file_id,
+            version=self.version,
+        )
+        await Wire.send_async(conn_sock, bytes(tell_header))
         use.echo_print('changing state to connection')  # debug
         self.state = STATE.CONNECTING
-
-        conn_sock = await connections.Connector.connect_peer(peer_obj)
-        await conn_sock.asendall(tell_header)
         use.echo_print('sent file header')  # debug
-        use.echo_print('waiting for connection', addr)  # debug
-        conn_sock, addr = await server_sock.aaccept()
-        server_sock.close()
-        del server_sock
-        use.echo_print('got connection', addr)  # debug
-        # conn_sock.set_loop(asyncio.get_running_loop())
-
-        use.echo_print('changing state to sending')  # debug
-        self.state = STATE.SENDING
-        # :todo: improve this making it a asyncio.task, taking longer time at send_files
-        self.file_handle, result = processmanager.submit(
-            asyncio.get_running_loop(),
-            processmanager.FILE,
-            self.file_list,
-            file_id,
-            conn_sock,
-            processmanager.SEND_COMMAND,
-        )
-
-        result = await result
+        connection = await connect.connect_to_peer(peer_obj, 0x02,)
+        with connection:
+            await self.authorize_connection(connection)
+            use.echo_print('changing state to sending')  # debug
+            self.state = STATE.SENDING
+            self.file_pool = PeerFilePool(
+                self.file_list,
+                _id=self.file_id,
+            )
         self.state = STATE.COMPLETED
-        print('completed sending', result)
-        return result
+        print('completed sending')
 
-    def _prepare_header(self, addr, file_id):
-        data = WireData(
-            header=const.CMD_RECV_FILE,
+    async def authorize_connection(self, connection):
+        handshake = WireData(
+            header=HEADERS.CMD_FILE_CONN,
             _id=get_this_remote_peer().id,
             version=self.version,
-            groups=len(self.file_list),
-            file_id=file_id,
-            addr=addr,
-        ).__bytes__()
-        data_size = struct.pack('!I',len(data))
-        return data_size + data
-
-    @staticmethod
-    def _prepare_socket() -> tuple[connect.Socket, tuple[str, int]]:
-        port = connect.get_free_port()
-        main_sock = const.PROTOCOL.create_async_sock(asyncio.get_running_loop(), const.IP_VERSION)
-        addr = (get_this_remote_peer().ip, port)
-        main_sock.bind(addr)
-        main_sock.listen(2)
-        return main_sock, addr
+            file_id=self.file_id,
+        )
+        await Wire.send_async(connection,bytes(handshake))
 
     async def status(self):
         ...
@@ -117,7 +99,7 @@ class FileSender:
 
 class FileReceiver:
 
-    def __init__(self, data:WireData):
+    def __init__(self, data):
         self.state = STATE.PREPARING
         self.peer_id = data.id
         self.group_count = data['groups']
@@ -147,4 +129,5 @@ class FileReceiver:
         return await connect.create_connection_async(self.addr)
 
 
-__all__ = ['FileSender', 'FileReceiver', 'get_id_for_file', 'open_file_selector', 'all_files']
+def new_file_recv_request(file_req: WireData):
+    ...
