18cf3db458e676c8319bfe48c16c7d53a9df1ff8
Ethicaldisguise <7862sadikd@gmail.com>
2024-10-27 20:56:35 +0530

    using eager task factory

--- src/core/eventloop.py
+++ src/core/eventloop.py
@@ -29,3 +29,6 @@ if sys.platform == 'win32':
     print('\033[32m setting up event loop',sys.platform, '\033[0m')  # debug
     _asyncio.set_event_loop_policy(CustomWindowsProactorEventLoopPolicy())
     # _asyncio.set_event_loop_policy(_asyncio.WindowsSelectorEventLoopPolicy())
+
+
+_asyncio.get_event_loop().set_task_factory(_asyncio.eager_task_factory)
