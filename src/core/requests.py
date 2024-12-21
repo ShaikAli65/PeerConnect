aae17e35cd0121e0628c16207fd14306a39b7066
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2024-12-22 02:36:32 +0530

    removed discovery functions

--- src/core/requests.py
+++ src/core/requests.py
@@ -1,14 +1,50 @@
 import asyncio
 import socket
 
-from src.avails import Wire, WireData, connect, const, unpack_datagram, use, wire
-from src.core import Dock, get_this_remote_peer, join_gossip, peers
-from src.core.transfers import REQUESTS_HEADERS, HEADERS
-from src.core import discover, get_gossip, transfers
+from src.avails import Wire, WireData, const, unpack_datagram, use, wire
+from src.core import Dock, _kademlia, get_gossip, get_this_remote_peer, peers
+from src.core.discover import search_network
+from src.core.gossip import join_gossip
+from src.core.transfers import HEADERS, REQUESTS_HEADERS
 from src.managers import filemanager, gossipmanager
 
 
-# communicating sequential processes CSP
+async def initiate():
+    loop = asyncio.get_running_loop()
+    server = _kademlia.get_new_kademlia_server()
+    await server.listen(port=const.PORT_NETWORK, interface=const.THIS_IP)
+    print("started kademlia endpoint at ", const.PORT_NETWORK)
+
+    node_addr = await search_network()
+    if node_addr is not None:
+        print("bootstrapping kademlia with", node_addr)  # debug
+        if await server.bootstrap(
+                [
+                    node_addr,
+                ]
+        ):
+            Dock.server_in_network = True
+
+    transport, proto = await loop.create_datagram_endpoint(
+        RequestsEndPoint,
+        # local_addr=('0.0.0.0' if const.IP_VERSION == socket.AF_INET else '::', const.PORT_REQ),
+        local_addr=(const.THIS_IP, const.PORT_REQ),
+        family=const.IP_VERSION,
+        proto=socket.IPPROTO_UDP,
+        allow_broadcast=True,
+    )
+    join_gossip(transport)
+
+    Dock.protocol = proto
+    Dock.requests_endpoint = transport
+    Dock.kademlia_network_server = server
+    f = use.wrap_with_tryexcept(server.add_this_peer_to_lists)
+    asyncio.create_task(f())
+    return server, transport, proto
+
+
+class RequestsRegistry:
+    ...
 
 
 class RequestsEndPoint(asyncio.DatagramProtocol):
@@ -97,86 +133,6 @@ class RequestsEndPoint(asyncio.DatagramProtocol):
         gossip_reply = wire.GossipMessage(req_data)
         reply = handler.request_arrived(gossip_reply, addr)
         Wire.send_datagram(self.transport, addr, reply)
-
-
-async def initiate():
-    loop = asyncio.get_running_loop()
-    server = discover.get_new_kademlia_server()
-    await server.listen(port=const.PORT_NETWORK, interface=const.THIS_IP)
-    print("started kademlia endpoint at ", const.PORT_NETWORK)
-
-    node_addr = await search_network()
-    if node_addr is not None:
-        print("bootstrapping kademlia with", node_addr)  # debug
-        if await server.bootstrap(
-            [
-                node_addr,
-            ]
-        ):
-            Dock.server_in_network = True
-
-    transport, proto = await loop.create_datagram_endpoint(
-        RequestsEndPoint,
-        # local_addr=('0.0.0.0' if const.IP_VERSION == socket.AF_INET else '::', const.PORT_REQ),
-        local_addr=(const.THIS_IP, const.PORT_REQ),
-        family=const.IP_VERSION,
-        proto=socket.IPPROTO_UDP,
-        allow_broadcast=True,
-    )
-    join_gossip(transport)
-
-    Dock.protocol = proto
-    Dock.requests_endpoint = transport
-    Dock.kademlia_network_server = server
-    f = use.wrap_with_tryexcept(server.add_this_peer_to_lists)
-    asyncio.create_task(f())
-    return server, transport, proto
-
-
-async def search_network():
-    ip, port = const.THIS_IP, const.PORT_REQ
-    this_id = get_this_remote_peer().id
-    ping_data = WireData(REQUESTS_HEADERS.NETWORK_FIND, this_id)
-    s = connect.UDPProtocol.create_async_server_sock(
-        asyncio.get_running_loop(),
-        (ip, port),
-        family=const.IP_VERSION
-    )
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    with s:
-        await ping_network(s, port, ping_data, times=2)  # debug
-        print('sent broadcast to network at port', ip, port)  # debug
-        return await wait_for_replies(s)
-
-
-async def ping_network(sock, port, req_payload, *, times=4):
-    for delay in use.get_timeouts(max_retries=times):
-        Wire.send_datagram(sock, ('<broadcast>', port), bytes(req_payload))
-        print("sent broadcast")  # debug
-        await asyncio.sleep(delay)
-
-
-async def wait_for_replies(sock, timeout=3):
-    print("waiting for replies at", sock)
-    while True:
-        try:
-            raw_data, addr = await asyncio.wait_for(Wire.recv_datagram_async(sock), timeout)
-        except asyncio.TimeoutError:
-            print(f'timeout reached at {use.func_str(wait_for_replies)}')
-            return None
-        try:
-            data = WireData.load_from(raw_data)
-            print("some data came ", data)  # debug
-        except TypeError as tp:
-            print(f"got error at {use.func_str(wait_for_replies)}", tp)
-            return
-        if addr == sock.getsockname():
-            print('ignoring echo')  # debug
-            continue
-        if data.match_header(REQUESTS_HEADERS.NETWORK_FIND_REPLY):
-            print("reply detected")  # debug
-            print("got some data", data)  # debug
-            return tuple(data['connect_uri'])
 
 
 async def end_requests():
