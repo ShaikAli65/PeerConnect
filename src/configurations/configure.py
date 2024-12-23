217b9195fd2aa68cb10fd450240a1d842f4f256c
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2024-12-23 18:06:45 +0530

    using f-strings in printing constants,removed PORT_FILE,

--- src/configurations/configure.py
+++ src/configurations/configure.py
@@ -1,10 +1,12 @@
 import configparser
+import ipaddress
 import os
 import socket
 from pathlib import Path
 
 import src.avails.connect as connect
 import src.avails.constants as const
+from src.avails import connect, constants, use
 
 
 def set_constants(config_map: configparser.ConfigParser) -> bool:
@@ -17,38 +19,41 @@ def set_constants(config_map: configparser.ConfigParser) -> bool:
         bool: True if configuration values were flip successfully, False otherwise.
     """
 
-    const.PAGE_SERVE_PORT = config_map.getint('NERD_OPTIONS', 'page_serve_port')
     const.PORT_THIS = config_map.getint('NERD_OPTIONS', 'this_port')
-    const.PORT_PAGE = config_map.getint('NERD_OPTIONS', 'page_port')
     const.PORT_REQ = config_map.getint('NERD_OPTIONS', 'req_port')
     const.PORT_NETWORK = config_map.getint('NERD_OPTIONS', 'network_port')
-    const.PORT_FILE = config_map.getint('NERD_OPTIONS', 'file_port')
+
+    const.PORT_PAGE = config_map.getint('NERD_OPTIONS', 'page_port')
+    const.PAGE_SERVE_PORT = config_map.getint('NERD_OPTIONS', 'page_serve_port')
+
     const.PROTOCOL = connect.TCPProtocol if config_map['NERD_OPTIONS']['protocol'] == 'tcp' else connect.UDPProtocol
     const.IP_VERSION = socket.AF_INET6 if config_map['NERD_OPTIONS']['ip_version'] == '6' else socket.AF_INET
+
     const.VERSIONS = {k.upper(): float(v) for k, v in config_map['VERSIONS'].items()}
+
     if const.IP_VERSION == socket.AF_INET6 and not socket.has_ipv6:
         const.IP_VERSION = socket.AF_INET
         print(
-            f"Error reading default_config.ini from set_constants() in {set_constants.__name__}()/{set_constants.__code__.co_filename}")
+            f"Error reading default_config.ini from {use.func_str(set_constants)}"
+        )
         return False
     return True
 
 
 def print_constants():
-    line = "{:<15} {:<10}"
 
     print_string = (
         f'\n:configuration choices{"=" * 32}\n'
-        f'{line.format("USERNAME", const.USERNAME)}\n'
-        f'{line.format("THIS_IP", const.THIS_IP)}\n'
-        f'{line.format("PROTOCOL", str(const.PROTOCOL))}\n'
-        f'{line.format("IP_VERSION", 6 if const.IP_VERSION == 23 else 4)}\n'
-        f'{line.format("SERVER_IP", const.SERVER_IP)}\n'
-        f'{line.format("PORT_THIS", const.PORT_THIS)}\n'
-        f'{line.format("SERVER_PORT", const.PORT_SERVER)}\n'
-        f'{line.format("NETWORK_PORT", const.PORT_NETWORK)}\n'
-        f'{line.format("PAGE_PORT", const.PORT_PAGE)}\n'
-        f'{line.format("PORT_REQ", const.PORT_REQ)}\n'
+        f'{"USERNAME": <15} : {const.USERNAME: <10}\n'
+        f'{"THIS_IP": <15} : {f"{const.THIS_IP}": <10}\n'
+        f'{"PROTOCOL": <15} : {f"{const.PROTOCOL}": <10}\n'
+        f'{"IP_VERSION": <15} : {ipaddress.ip_address(const.THIS_IP).version: <10}\n'
+        f'{"SERVER_IP": <15} : {f"{const.SERVER_IP}": <10}\n'
+        f'{"PORT_THIS": <15} : {const.PORT_THIS: <10}\n'
+        f'{"SERVER_PORT": <15} : {const.PORT_SERVER: <10}\n'
+        f'{"NETWORK_PORT": <15} : {const.PORT_NETWORK: <10}\n'
+        f'{"PAGE_PORT": <15} : {const.PORT_PAGE: <10}\n'
+        f'{"PORT_REQ": <15} : {const.PORT_REQ: <10}\n'
         f'{"=" * 56}\n'
     )
     with const.LOCK_PRINT:
@@ -79,3 +84,12 @@ def set_paths():
     except OSError as e:
         # error_log(f"Error creating directory: {e} from set_paths() at line 70 in core/constants.py")
         const.PATH_DOWNLOAD = os.path.join(const.PATH_CURRENT, 'fallbacks')
+
+
+def validate_ports(ip):
+    ports_list = [const.PORT_THIS, const.PORT_PAGE, const.PORT_REQ,]
+    for i, port in enumerate(ports_list):
+        if not connect.is_port_empty(port, ip):
+            ports_list[i] = connect.get_free_port(ip)
+            use.echo_print(f"Port is not empty. choosing another port: {ports_list[i]}")
+    const.PORT_THIS, const.PORT_PAGE, const.PORT_REQ = ports_list
