d7baa32c391f9d316a7577dc3357662870c77978
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2024-12-27 02:52:53 +0530

    related to transport classes that wrap existing sockets or DatagramTransports which help in multiplexing at requests endpoint

---
+++ src/core/transfers/transports.py
@@ -0,0 +1,58 @@
+import struct
+from asyncio import BaseTransport
+from typing import override
+
+from src.avails import Wire, WireData
+from src.core.transfers import REQUESTS_HEADERS
+
+
+class RequestsTransport(BaseTransport):  # just for type hinting
+    """Wraps datagram to multiplex at Requests Endpoint
+
+    Other services use to define service specific trigger header
+    that gets added to the message when it is sent through Requests endpoint
+    which is further used to detect and multiplex to different registered dispatchers
+
+    Note:
+        no need to use this as **Wire.send_*(self.transport)**, that is only for bare sockets
+
+    Usage:
+        >>> class Subclass(RequestsTransport):
+        >>>     trigger = b'\x11'  # some code of one byte
+    """
+
+    __slots__ = 'transport', 'trigger'
+    _trigger = b''
+
+    def __init__(self, transport, _event_trigger_header=None):
+        super().__init__()
+        self.transport = transport
+        self.trigger = self._trigger or _event_trigger_header
+
+    def sendto(self, data: bytes, addr: tuple = None):
+        data_size = struct.pack('!I', len(req_data_in_bytes := bytes(data)))
+        data_to_send = self._trigger + data_size + req_data_in_bytes
+        return self.transport.sendto(data_to_send, addr)
+
+    def close(self):
+        return self.transport.close()
+
+
+class KademliaTransport(RequestsTransport):
+    __slots__ = ()
+    _trigger = REQUESTS_HEADERS.KADEMLIA
+
+    @override
+    def sendto(self, data: bytes, addr: tuple[str, int] = None):
+        formatted = bytes(WireData(data=data))
+        return super().sendto(formatted, addr)
+
+
+class DiscoveryTransport(RequestsTransport):
+    __slots__ = ()
+    _trigger = REQUESTS_HEADERS.DISCOVERY
+
+
+class GossipTransport(RequestsTransport):
+    __slots__ = ()
+    _trigger = REQUESTS_HEADERS.GOSSIP
