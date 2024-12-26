f5ddbe83e9ce356330d8c22b6a58a79c9ab797ee
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2024-12-27 02:50:56 +0530

    added compatibitily to Dispatchers, removed server_in_network, protocol, these two are not of any use state_handle renamed to state_manager_handle, added a Enum to reference different types of dispatchers, new function to retrieve dispatchers: ``get_dispatcher``

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,23 +1,31 @@
 import asyncio as _asyncio
+from enum import IntEnum
 from typing import Optional, TYPE_CHECKING
 
 from src.avails import (
-    PeerDict as _PeerDict,
+    BaseDispatcher, PeerDict as _PeerDict,
     RemotePeer as _RemotePeer,
     SocketCache as _SocketCache
 )
 
 
+class DISPATCHS(IntEnum):
+    REQUESTS = 1
+    DISCOVER = 2
+    GOSSIP = 3
+
+
 class Dock:
     connected_peers = _SocketCache()
     peer_list = _PeerDict()
-    server_in_network = False
-    state_handle = None
-    protocol = None
+    state_manager_handle = None
     global_gossip = None
     _this_object: Optional[_RemotePeer] = None
     kademlia_network_server = None
     finalizing = _asyncio.Event()
+    requests_endpoint: Optional[_asyncio.DatagramTransport] = None
+    dispatchers: dict[DISPATCHS, BaseDispatcher] = {}
+
     if TYPE_CHECKING:
         from src.core.transfers import RumorMongerProtocol
         from src.core._kademlia import PeerServer
@@ -25,9 +33,7 @@ class Dock:
 
         global_gossip: RumorMongerProtocol
         kademlia_network_server: PeerServer
-        state_handle: StateManager
-
-    requests_endpoint: Optional[_asyncio.DatagramTransport] = None
+        state_manager_handle: StateManager
 
 
 def get_this_remote_peer():
@@ -38,6 +44,9 @@ def get_gossip():
     return Dock.global_gossip
 
 
+def get_dispatcher(dispatcher_id):
+    return Dock.dispatchers[dispatcher_id]
+
+
 def set_current_remote_peer_object(remote_peer):
     Dock._this_object = remote_peer
-
