3389b680d6e2fbe9290086cc84d93e64da2ef2d9
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2024-12-28 02:50:56 +0530

    change of Handler classes into Closure functions, removed use.NotInUse marked functions

--- src/core/discover.py
+++ src/core/discover.py
@@ -1,47 +1,57 @@
-import asyncio
 import socket
-from typing import Literal
 
-from src.avails import QueueMixIn, Wire, WireData, const, use
+from src.avails import QueueMixIn, WireData, const, use
 from src.avails.bases import BaseDispatcher, RequestEvent
 from src.avails.connect import ipv4_multicast_socket_helper, ipv6_multicast_socket_helper
 from src.core import get_this_remote_peer
-from src.core.transfers import REQUESTS_HEADERS
+from src.core.transfers import DISCOVERY
 from src.core.transfers.transports import DiscoveryTransport
+
+
+def DiscoveryReplyHandler(kad_server):
+    async def handle(event: RequestEvent):
+        connect_address = tuple(event.request['connect_uri'])
+        print("[KADEMLIA] bootstrapping kademlia")
+        return await kad_server.bootstrap([connect_address])
+
+    return handle
+
+
+def DiscoveryRequestHandler(discovery_transport):
+    async def handle(event: RequestEvent):
+        req_packet = event.request
+        print("replying to req:", req_packet.body)
+        this_rp = get_this_remote_peer()
+        data_payload = WireData(
+            header=DISCOVERY.NETWORK_FIND_REPLY,
+            _id=this_rp.id,
+            connect_uri=this_rp.req_uri
+        )
+        return discovery_transport.sendto(bytes(data_payload), tuple(req_packet['reply_addr']))
+
+    return handle
 
 
 class DiscoveryDispatcher(QueueMixIn, BaseDispatcher):
     def __init__(self, transport, stopping_flag):
-        self.registry = {
-            REQUESTS_HEADERS.NETWORK_FIND: self.find_request,
-        }
         super().__init__(transport=DiscoveryTransport(transport), stop_flag=stopping_flag)
 
     async def submit(self, event: RequestEvent):
-        wire_data = WireData.load_from(event.request)  # we need to extract data as request dispatcher
+        wire_data = event.request
         handle = self.registry[wire_data.header]
-        await handle(wire_data)
-
-    async def find_request(self, req_packet: WireData):
-        print("replying to ", req_packet.body)
-        this_rp = get_this_remote_peer()
-        data_payload = WireData(
-            header=REQUESTS_HEADERS.NETWORK_FIND_REPLY,
-            _id=this_rp.id,
-            connect_uri=this_rp.req_uri
-        )
-        return self.transport.sendto(bytes(data_payload), tuple(req_packet['reply_addr']))
+        print("[DISCOVERY] dispatching request with id", event.root_code, 'to', wire_data)
+        await handle(event)
 
 
 async def search_network(transport, broad_cast_addr, multicast_addr):
     this_rp = get_this_remote_peer()
-    ping_data = WireData(REQUESTS_HEADERS.NETWORK_FIND, this_rp.id, reply_addr=this_rp.req_uri)
+    ping_data = WireData(DISCOVERY.NETWORK_FIND, this_rp.id, reply_addr=this_rp.req_uri)
 
     if const.USING_IP_V4:
-        async for _ in use.async_timeouts(const.DISCOVER_RETRIES):
+        async for _ in use.async_timeouts(max_retries=const.DISCOVER_RETRIES):
             transport.sendto(bytes(ping_data), broad_cast_addr)
 
-    async for _ in use.async_timeouts(const.DISCOVER_RETRIES):
+    async for _ in use.async_timeouts(max_retries=const.DISCOVER_RETRIES):
         transport.sendto(bytes(ping_data), multicast_addr)
 
 
@@ -54,72 +64,3 @@ def _add_multicast(multicast_sock, addr):
         ipv4_multicast_socket_helper(multicast_sock, addr)
     else:
         ipv6_multicast_socket_helper(multicast_sock, addr)
-
-
-@use.NotInUse
-async def broadcast_search(broadcast_sock, broadcast_addr: tuple[Literal['<broadcast>'] | str, int], req_payload):
-    broadcast_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    f = _request_response_loop(
-        broadcast_sock,
-        broadcast_addr,
-        req_payload,
-        const.DISCOVER_RETRIES
-    )
-    answer = await f
-    broadcast_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 0)
-    return answer
-
-
-@use.NotInUse
-async def _request_response_loop(sock, send_addr, req_payload, retry_count):
-    reply_fut = asyncio.get_event_loop().create_future()
-
-    async def _reply_waiter():
-
-        def is_a_valid_response(_reply, _addr):
-            if _addr == sock.getsockname():
-                print('ignoring echo')  # debug
-                return False
-            if _reply.match_header(REQUESTS_HEADERS.NETWORK_FIND_REPLY):
-                return True
-
-        print("listening for discover replies on", sock.getsockname())
-        while True:
-            raw_data, addr = await Wire.recv_datagram_async(sock)
-            reply = WireData.load_from(raw_data)
-            print("got", reply, addr)
-            if is_a_valid_response(reply, addr):
-                print("reply detected", reply)  # debug
-                result = tuple(reply['connect_uri'])
-                reply_fut.set_result(result)  # we got address to bootstrap with
-                return result
-
-    task = asyncio.create_task(_reply_waiter())
-
-    for timeout in use.get_timeouts(max_retries=retry_count):
-        Wire.send_datagram(sock, send_addr, bytes(req_payload))
-        print(f"sent{send_addr}" + ("=" * 80))
-        try:
-            resp = await asyncio.wait_for(asyncio.shield(reply_fut), timeout)
-            print("got" + '=>', resp)
-            return resp
-        except TimeoutError:
-            print("timeout")
-
-    task.cancel()
-
-
-@use.NotInUse
-async def multicast_search(multicast_sock, multicast_addr, req_payload):
-    if const.USING_IP_V4:
-        ipv4_multicast_socket_helper(multicast_sock, multicast_addr)
-    else:
-        ipv6_multicast_socket_helper(multicast_sock, multicast_addr)
-
-    answer = await _request_response_loop(
-        multicast_sock,
-        multicast_addr,
-        req_payload,
-        const.DISCOVER_RETRIES
-    )
-    return answer
