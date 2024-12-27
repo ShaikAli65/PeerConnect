5df6d21caf71e2176fde4a4e93166615539d7383
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2024-12-28 02:53:24 +0530

    a little bit of printing and added multicast_port and multicast_addr

--- tests/multicast_stub.py
+++ tests/multicast_stub.py
@@ -6,19 +6,28 @@ class Multicast(asyncio.DatagramProtocol):
         self.all_peers = set()
 
     def connection_made(self, transport):
+        print("server up and running", transport.get_extra_info('socket'))
         self.transport = transport
 
     def datagram_received(self, data, addr):
-        self.all_peers.add(addr)
-        for peer in self.all_peers:
+        print('new message arrived', addr)
+        print("forwarding to", end=' ')
+        for peer in (self.all_peers - {addr}):
+            print(addr, end=' ')
             self.transport.sendto(data, peer)
+        print('')
+        self.all_peers.add(addr)
 
 
 async def main():
+    multicast_ip = '127.0.0.1'
+    multicast_port = 4000
     loop = asyncio.get_running_loop()
     await loop.create_datagram_endpoint(
         Multicast,
+        local_addr=(multicast_ip, multicast_port)
     )
+    await asyncio.Event().wait()
 
 
 if __name__ == "__main__":
