48806252b149b03a92f79d22858e195a75e3b18a
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2024-12-28 02:51:25 +0530

    new exceptions::
        WebSocketRegistryReStarted
        InvalidPacket

--- src/avails/exceptions.py
+++ src/avails/exceptions.py
@@ -1,4 +1,10 @@
-
 class DispatcherFinalizing(Exception):
     pass
 
+
+class WebSocketRegistryReStarted(Exception):
+    """WebSocketRegistry already started"""
+
+
+class InvalidPacket(Exception):
+    """Ill formed Packet"""
