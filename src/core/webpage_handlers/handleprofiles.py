123f0cd42740f32e13751a868321c767aa1d38d2
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2025-01-02 01:19:56 +0530

    change of profile managing schema, now new profile id get created at frontend and we don't use the key from incoming data rather we choose to go with internal user.name when creating profile

--- src/core/webpage_handlers/handleprofiles.py
+++ src/core/webpage_handlers/handleprofiles.py
@@ -5,18 +5,19 @@ from src.managers import (
     ProfileManager,
     all_profiles,
     get_profile_from_profile_file_name,
-    set_current_profile,
+    refresh_profile_list, set_current_profile,
 )
 
 
 async def align_profiles(signal_data: DataWeaver):
     further_data = await send_profiles()
     await configure_further_profile_data(further_data)
+    refresh_profile_list()
 
 
 def send_profiles():
     userdata = DataWeaver(header=HEADERS.HANDLE_PEER_LIST, content=all_profiles())
-    use.echo_print("::profiles sent")
+    use.echo_print("::[HANDLE PROFILES] profiles sent")
     return dispatch_data(userdata, expect_reply=True)
 
 
@@ -41,18 +42,20 @@ async def configure_further_profile_data(profiles_data):
     if removed_profiles := set(all_profiles()) - set(profiles_data):
         for profile_file_name in removed_profiles:
             ProfileManager.delete_profile(profile_file_name)
-        ProfileManager.PROFILE_LIST = [
-            profile
-            for profile in ProfileManager.PROFILE_LIST
-            if profile.username not in removed_profiles
-        ]
         use.echo_print("deleted profiles :", removed_profiles)
 
-    for profile_name, profile_settings in profiles_data.items():
-        profile_object = get_profile_from_profile_file_name(profile_name)
+    for may_be_profile_name, profile_settings in profiles_data.items():
+        profile_object = get_profile_from_profile_file_name(may_be_profile_name)
         if profile_object is None:
-            ProfileManager.add_profile(profile_name, profile_settings)
-            use.echo_print("added profile :", profile_name, profile_settings)
+            profile_settings['USER']['id'] = int(profile_settings['USER']['id'])  # = new_remote_peer_id()
+            profile_name = profile_settings['USER']['name']
+
+            # new profile does not have any id associated with it
+            try:
+                ProfileManager.add_profile(profile_name, profile_settings)
+            except Exception as e:
+                print("-" * 80, e)
+            use.echo_print("[HANDLE PROFILE] added profile :", may_be_profile_name, profile_settings)
             continue
 
         for header, content in profile_settings.items():
