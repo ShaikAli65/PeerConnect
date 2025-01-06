34aa7ea5627029ea5fb82c577f993c32c492e14e
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2025-01-06 22:38:28 +0530

    using queue loggers and added exit stack mechanism for closing

--- src/managers/logmanager.py
+++ src/managers/logmanager.py
@@ -1,14 +1,31 @@
-import logging.config
 import json
+import logging.config
+import queue
+from contextlib import contextmanager
 from pathlib import Path
 
 from src.avails import const
 
+log_queue = queue.SimpleQueue()
 
+
+@contextmanager
 def initiate():
     with open(const.PATH_LOG_CONFIG) as fp:
         log_config = json.load(fp)
+    log_file_name = log_config["log_file_name"]
+    log_file_path = Path(const.PATH_LOG, log_file_name)
+    log_config["handlers"]["rfile_handler"]["filename"] = log_file_path
 
-    log_config["handlers"]["file"]["filename"] = Path(const.PATH_LOG, 'new.log')
+    log_config["handlers"]["file"]["filename"] = log_file_path
 
     logging.config.dictConfig(log_config)
+    q_handler = logging.getHandlerByName("queue_handler")
+    queue_listener = q_handler.listener
+    queue_listener.start()
+    yield
+    queue_listener.stop()
+    for handler in queue_listener.handlers:
+        handler.close()
+    print("closing logging")
+
