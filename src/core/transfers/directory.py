95176aa4c20ebdd97ebbe9237516a132919be1c5
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2025-01-12 23:31:32 +0530

    new directory transfer protocol, to be documented later,

--- src/core/transfers/directory.py
+++ src/core/transfers/directory.py
@@ -1,19 +1,21 @@
 import asyncio
+import logging
 import struct
+from contextlib import aclosing
 from pathlib import Path
-from typing import NamedTuple
 
-import tqdm
 import umsgpack
 
-from src.core.transfers import FileItem, TransferState, fileobject, recv_actual_file
+from src.avails import connect, const
+from src.avails.exceptions import TransferIncomplete
+from src.avails.status import StatusMixIn
+from src.avails.useables import LONG_INT, recv_int
+from src.core.transfers import FileItem, TransferState, recv_file_contents, send_actual_file
 
-END_DIR_WITH = '/'
+_logger = logging.getLogger(__name__)
 
-
-class Ack(NamedTuple):
-    status: bool
-    seeked: int
+_FILE_CODE = b'\x00'
+_PATH_CODE = b'\x01'
 
 
 def rename_directory_with_increment(root_path: Path, relative_path: Path):
@@ -47,126 +49,84 @@ def rename_directory_with_increment(root_path: Path, relative_path: Path):
     return new_path
 
 
-class DirectorySender:
-    def __init__(self, item_queue, ack_queue, send_func, recv_func, root_path):
-        self.item_queue: asyncio.Queue[FileItem | Path] = item_queue
-        self.ack_queue: asyncio.Queue[Ack] = ack_queue
-        self.send_func = send_func
-        self.recv_func = recv_func
-        self.root_path: Path = root_path
+class Sender(StatusMixIn):
+    timeout = 5
 
-    async def initiate(self):
-        while True:
-            item = await self.item_queue.get()
-            if isinstance(item, FileItem):
-                await self.send_func(b'\x00')
-                ack = await self._send_file_item(item)
-                assert await self.recv_func(1) == b'\x00'
-            else:
-                await self.send_func(b'\x01')
-                ack = await self._send_path(item, END_DIR_WITH)
-                assert await self.recv_func(1) == b'\x01'
-
-            await self.ack_queue.put(ack)
-
-    async def _send_path(self, path: Path, end_with):
-        path = path.relative_to(self.root_path)
-        path_to_send = str(path)
-        path_len = struct.pack('!I', len(path_to_send))
-        await self.send_func(path_len)
-        await self.send_func(path_to_send.encode())
-        return path_to_send
-
-    async def _send_file_item(self, file_item: FileItem):
-        file_path = str(file_item.path.relative_to(self.root_path))
-        file_packet = umsgpack.dumps((file_path, file_item.size))
-        file_packet = struct.pack('!I', len(file_packet)) + file_packet
-
-        await self.send_func(file_packet)  # possible : any sort of socket/connection errors
-
-        send_progress = tqdm.tqdm(
-            range(file_item.size),
-            f" sending {file_item.name[:17] + '...' if len(file_item.name) > 20 else file_item.name} ",
-            unit="B",
-            unit_scale=True,
-            unit_divisor=1024
-        )
-        # send_progress = await fileobject.send_file_setup(self.send_func, file=file_item)
-        try:
-            await fileobject.send_actual_file(self.send_func, file_item, send_progress)
-        except ValueError as ve:
-            import errno
-            if ve.args[0] == "cannot mmap an empty file":
-                pass
-
-        send_progress.close()
-        return file_item
-
-
-class DirectoryReceiver:
-    ...
-
-
-class DirectorySenderManager:
-    def __init__(self, root_path, transfer_id, send_func, recv_func, buffer_items=5):
+    def __init__(self, root_path, transfer_id, send_func, recv_func, yield_freq=10):
         """
         Args:
             root_path(Path): root path to read from and start the transfer
             transfer_id(str): transfer id that synchronized both sides
-            send_func(Callable[[bytes],Coroutine[None, None, None]): function to call upon to send file data when ready
-            buffer_items(int): maximum no of items to buffer for confirmations from sender object
+            send_func(connect.Sender): function to call upon to send file data when ready
+            recv_func(connect.Receiver) : function to call upon to receive file data when ready
+            yield_freq(int): status yield frequency
         """
         self.state = TransferState.PREPARING
         self.root_path = root_path
         self.current_file = None
         self.id = transfer_id
-        self.send_func = send_func
+        self.send_bytes = send_func
+        self.recv_bytes = recv_func
         self.proceed_flag = asyncio.Event()
-        self.item_queue = asyncio.Queue[Path | FileItem](maxsize=buffer_items)
-        self.ack_queue = asyncio.Queue[Ack](maxsize=buffer_items)
-        self.transferred_directory_paths = []
         self.dir_iterator = self.root_path.rglob('*')
-        self.to_be_acked_items = []
         self.current_dir_sending_state_info = None
-        self.dir_sender = DirectorySender(
-            self.item_queue,
-            self.ack_queue,
-            send_func,
-            recv_func,
-            self.root_path,
-        )
+        super().__init__(yield_freq)
 
     async def start(self):
-        sender_task = asyncio.create_task(self.dir_sender.initiate())
-        try:
-            for item in self.dir_iterator:
-                if self.proceed_flag.is_set():
-                    break
-
-                if item.is_file():
-                    item_to_put_in_queue = FileItem(path=item, seeked=0)
-                elif item.is_dir():
-                    item_to_put_in_queue = item
-                else:
-                    continue
-                print("putting item in queue", item_to_put_in_queue)
-
-                if self.item_queue.full():
-                    await self._process_acks()
-
-                await self.item_queue.put(item_to_put_in_queue)
-                self.to_be_acked_items.append(item_to_put_in_queue)
-            await self._process_acks()
-        finally:
-            await sender_task
-
-    async def _process_acks(self):
-        while not self.proceed_flag.is_set():
-            if not self.ack_queue.empty():
-                ack = await self.ack_queue.get()
-                # print("received ack ", ack)
-            else:
+        for item in self.dir_iterator:
+            if self.proceed_flag.is_set():
                 break
+            await self.send_bytes(b'\x01')  # code to inform that there are more files to get
+
+            await self._send_code(item)
+            yield item
+            if item.is_file():
+                file_item = FileItem(path=item, seeked=0)
+
+                await self.send_bytes(struct.pack('!Q', file_item.size))
+
+                if file_item.size <= 0:
+                    continue
+
+                async with aclosing(
+                        self._send_file(file_item)
+                ) as sender:
+                    async for i in sender:
+                        yield i
+
+        await self.send_bytes(b'\x00')  # code to inform end of transfer
+
+    async def _send_file(self, file_item):
+
+        self.status_setup(f"[DIR] sending : {file_item}", file_item.seeked, file_item.size)
+        async with aclosing(
+                send_actual_file(self.send_bytes, file_item)
+        ) as file_sender:
+            async for seeked in file_sender:
+                self.update_status(seeked)
+                if self.should_yield():
+                    yield file_item, seeked
+
+    async def _send_code(self, path: Path):
+        code = None
+        if path.is_dir():
+            code = _PATH_CODE
+        elif path.is_file():
+            code = _FILE_CODE
+
+        rel_path = path.relative_to(self.root_path)
+        parent = rel_path.parent
+        name = rel_path.name
+
+        if const.IS_WINDOWS:
+            parent = parent.as_posix()
+        if const.IS_LINUX:
+            name = name.replace('\\', '_')
+
+        dumped_code = umsgpack.dumps((parent, name, code))
+        await self.send_bytes(struct.pack('!I', len(dumped_code)))
+        await self.send_bytes(dumped_code)
+        return parent, name, code
 
     def stop(self):
         pass
@@ -178,56 +138,128 @@ class DirectorySenderManager:
         pass
 
 
-class DirectoryReceiveManager:
-    def __init__(self, transfer_id, recv_func, send_func, download_path):
+class Receiver(StatusMixIn):
+    def __init__(self, transfer_id, recv_func, send_func, download_path, status_yield_freq):
         self.id = transfer_id
-        self.recv_func = recv_func
-        self.send_func = send_func
+        self.get_bytes = recv_func
+        self.send_bytes = send_func
         self.stop_flag = asyncio.Event()
         self.download_path = download_path
+        self.state = TransferState.PREPARING
+        self.logger_prefix = f"DIR[{self.id}]"
+        self._current_item = None
+        super().__init__(status_yield_freq)
 
     async def start(self):
-        while True:
-            code = await self.recv_func(1)
-            if code == b'':
-                raise ConnectionResetError()
+        self.state = TransferState.RECEIVING
+        stopper = self.stop_flag.is_set
+        while not stopper():
+            what = await self.get_bytes(1)
+            if what == b'\x00':  # no more files to get
+                break
 
-            if code == b'\x00':
-                await self._recv_file()
-            elif code == b'\x01':
-                abs_path = await self._recv_path()
-                abs_path.mkdir(parents=True, exist_ok=True)
+            parent, item_name, code = await self._recv_code()
+            print('received code', (parent, item_name, code))  # debug
+            full_path = Path(self.download_path, parent, item_name)
 
-            await self.send_func(code)
+            if code == _FILE_CODE:
+                file_item = await self._recv_file_item(full_path)
+                async with aclosing(self._handle_file(file_item)) as f:
+                    async for t in f:
+                        yield t
 
-    async def _recv_path(self):
-        rel_path_size = struct.unpack('!I', await self.recv_func(4))[0]
-        rel_path = await self.recv_func(rel_path_size)
-        abs_path = Path(self.download_path, rel_path.decode())
-        return abs_path
+            elif code == _PATH_CODE:
+                full_path.mkdir(parents=True)
+                print("creating directory", full_path)  # debug
+                yield full_path, None
 
-    async def _recv_file(self):
-        file_item_size = struct.unpack('!I', await self.recv_func(4))[0]
-        file_path, file_size = umsgpack.loads(await self.recv_func(file_item_size))
-        file_item = FileItem(Path(self.download_path, file_path), seeked=0)
-        file_item.size = file_size
-        progress = tqdm.tqdm(
-            range(file_item.size),
-            f"::receiving {file_item.name[:17] + '...' if len(file_item.name) > 20 else file_item.name} ",
-            unit="B",
-            unit_scale=True,
-            unit_divisor=1024
+            # await self._echo_code(code)
+
+    async def _recv_code(self):
+        try:
+            code_len = await recv_int(self.get_bytes)
+            parent, item, code = umsgpack.loads(await self.get_bytes(code_len))
+            return parent, item, code
+        except (struct.error, umsgpack.UnpackException) as exp:
+            self.state = TransferState.PAUSED
+            _logger.error(f"{self.logger_prefix} failed to receive item code, changed state to {self.state}",
+                          exc_info=True)
+            raise TransferIncomplete("failed to receive item code") from exp
+
+    async def _recv_file_item(self, file_path: Path):
+        try:
+            size = await recv_int(self.get_bytes, type=LONG_INT)
+        except ValueError as ve:
+            self.state = TransferState.PAUSED
+            _logger.error(f"{self.logger_prefix} failed to receive file size, changed state to {self.state}",
+                          exc_info=True)
+            raise TransferIncomplete("failed to receive file size") from ve
+
+        file_item = FileItem(file_path, 0)
+        file_item.size = size
+        return file_item
+
+    async def _handle_file(self, file_item):
+
+        if file_item.size <= 0:
+            print("[DIR] empty file, creating", file_item)
+            file_item.path.touch()
+            return
+
+        self.status_setup(f"[DIR] receiving file: {file_item}", file_item.seeked, file_item.size)
+
+        receiver = recv_file_contents(self.get_bytes, file_item)
+        try:
+            async with aclosing(receiver) as receiver:
+                async for received_size in receiver:
+                    self.update_status(received_size)
+                    if self.should_yield():
+                        yield file_item, received_size
+
+        except TransferIncomplete:
+            self.state = TransferState.PAUSED
+            self._reraise_error_and_log(f"file contents, {file_item}")
+
+    async def _echo_code(self, code):
+        try:
+            await self.send_bytes(code)  # echo back code to confirm
+        except OSError as oe:
+            self.state = TransferState.PAUSED
+            _logger.error(f"{self.logger_prefix} failed to send confirmation code, changed state to {self.state}")
+            raise TransferIncomplete("failed to send confirmation code") from oe
+
+    def pause(self):
+        self.state = TransferState.PAUSED
+        self.send_bytes.pause()
+        self.get_bytes.pause()
+
+    def resume(self):
+        self.state = TransferState.RECEIVING
+        self.send_bytes.resume()
+        self.get_bytes.resume()
+
+    async def continue_transfer(self):
+        self.state = TransferState.RECEIVING
+        self.stop_flag.clear()
+
+        if self._current_item:
+            await self.send_bytes(b'\x01')
+            packed_seeked = struct.pack('!Q', self._current_item.seeked)
+            await self.send_bytes(packed_seeked)
+            # synchronizing file state
+
+        await self.send_bytes(b'\x00')
+        # resuming transfer
+        async with aclosing(self.start()) as recv:
+            async for status in recv:
+                yield status
+
+    def _reraise_error_and_log(self, msg):
+        _logger.error(
+            f"{self.logger_prefix} failed to receive {msg}, changed state to {self.state}",
+            exc_info=True
         )
-        await recv_actual_file(self.stop_flag.is_set, self.recv_func, file_item, progress)
+        raise
 
     async def stop(self):
         pass
-
-    async def pause(self):
-        self.stop_flag.set()
-
-    async def continue_transfer(self):
-        self.stop_flag.clear()
-        packed_seeked = struct.pack('!Q', self._current_item.seeked)
-        await self.send_func(packed_seeked)
-
