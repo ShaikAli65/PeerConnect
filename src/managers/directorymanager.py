30f538a861ba7097e540e1a0b35e7604e5029386
Shaik Ali <89526384+ShaikAli65@users.noreply.github.com>
2025-01-12 23:32:17 +0530

    new directory transfer protocol, to be documented later,

--- src/managers/directorymanager.py
+++ src/managers/directorymanager.py
@@ -3,20 +3,19 @@ import itertools
 import logging
 import os
 import pathlib
-import socket
 import struct
 import traceback
+from contextlib import aclosing, asynccontextmanager
 from pathlib import Path
 
 from src.avails import TransfersBookKeeper, Wire, WireData, connect, const, get_dialog_handler, use
 from src.avails.events import ConnectionEvent
-from src.core import Dock, get_this_remote_peer, peers
+from src.avails.exceptions import TransferRejected
+from src.core import Dock, get_this_remote_peer
 from src.core.handles import TaskHandle
-from src.core.transfers import FileItem, HEADERS
-from src.core.transfers.directory import DirectoryReceiveManager, DirectorySenderManager, \
+from src.core.transfers import FileItem, HEADERS, shorten_path
+from src.core.transfers.directory import Receiver, Sender, \
     rename_directory_with_increment
-
-END_DIR_WITH = '/'
 
 transfers_book = TransfersBookKeeper()
 _logger = logging.getLogger(__name__)
@@ -24,15 +23,12 @@ _logger = logging.getLogger(__name__)
 
 async def open_dir_selector():
     loop = asyncio.get_running_loop()
-    result = loop.run_in_executor(None, get_dialog_handler().open_directory_dialog_window)
+    result = loop.run_in_executor(None, get_dialog_handler().open_directory_dialog_window)  # noqa
     return await result
 
 
-async def new_directory_send_transfer(peer_id):
-    dir_path = await open_dir_selector()
-    if not dir_path:
-        print("cancelling dir send request, no directory specified")
-        return
+@asynccontextmanager
+async def send_directory(remote_peer, dir_path):
     dir_path = Path(dir_path)
     transfer_id = transfers_book.get_new_id()
     dir_recv_signal_packet = WireData(
@@ -41,9 +37,6 @@ async def new_directory_send_transfer(peer_id):
         transfer_id=transfer_id,
         dir_name=dir_path.name,
     )
-    remote_peer = await peers.get_remote_peer_at_every_cost(peer_id)
-    if not remote_peer:
-        raise Exception(f"cannot find remote peer object for given id{peer_id}")
     # from src.core.connections import Connector
     # connection = await Connector.get_connection(remote_peer)
 
@@ -54,32 +47,37 @@ async def new_directory_send_transfer(peer_id):
         retries=3
     )
 
+    timeout = 5
     with connection:
         Wire.send(connection, bytes(dir_recv_signal_packet))
+        await _get_confirmation(connection, timeout)
 
-        try:
-            confirmation = await asyncio.wait_for(connection.arecv(1), timeout=300)
-            if confirmation == b'\x00':
-                _logger.info("not sending directory, other end rejected")
-                return
-        except asyncio.TimeoutError:
-            _logger.info("not sending directory, did not receive confirmation within 3 seconds")
-            return
-        except ConnectionResetError:
-            _logger.error("not sending directory", exc_info=True)
-
-        sender = DirectorySenderManager(
+        sender = Sender(
             dir_path,
             transfer_id,
-            connection.asendall,
-            connection.arecv,
+            connect.Sender(connection),
+            connect.Receiver(connection),
         )
+
         _logger.info(f"sending directory: {dir_path} to {remote_peer}")
-        try:
-            await sender.start()
-        except Exception as e:
-            traceback.print_exc()
-            print(e)
+        async with aclosing(sender.start()) as s:
+            yield s
+
+        _logger.info(f"completed sending directory {dir_path} to {remote_peer}")
+
+
+async def _get_confirmation(connection, timeout):
+    try:
+        confirmation = await asyncio.wait_for(connection.arecv(1), timeout=timeout)
+        if confirmation == b'\x00':
+            _logger.info("not sending directory, other end rejected")
+            raise TransferRejected()
+    except asyncio.TimeoutError:
+        _logger.info(f"not sending directory, did not receive confirmation within {timeout} seconds")
+        raise
+    except ConnectionResetError:
+        _logger.error("not sending directory", exc_info=True)
+        raise
 
 
 def pause_transfer(peer_id, transfer_id):
@@ -99,85 +97,35 @@ def DirConnectionHandler():
         connection = event.transport.socket
         dir_name = event.handshake.body['dir_name']
         dir_path = rename_directory_with_increment(const.PATH_DOWNLOAD, Path(dir_name))
-        receiver = DirectoryReceiveManager(
+
+        sender = connect.Sender(connection)
+        recv = connect.Receiver(connection)
+        receiver = Receiver(
             transfer_id,
-            connection.arecv,
-            connection.asendall,
+            recv,
+            sender,
             dir_path,
+            const.TRANSFER_STATUS_FREQ
         )
-        await connection.asendall(b'\x01')
-        transfers_book.add_to_current(transfer_id, receiver)
-        _logger.info(f"receiving directory from {Dock.peer_list.get_peer(event.handshake.peer_id)} peer_id={event.handshake.peer_id}")
         try:
-            await receiver.start()
-            _logger.info(f"directory received from {Dock.peer_list.get_peer(event.handshake.peer_id)}")
-            transfers_book.add_to_completed(transfer_id, receiver)
+            with connection:
+                await sender(b'\x01')
+                transfers_book.add_to_current(transfer_id, receiver)
+                _logger.info(
+                    f"receiving directory from {Dock.peer_list.get_peer(event.handshake.peer_id)}, saving at {shorten_path(dir_path, 20)}"
+                )
+                async with aclosing(receiver.start()) as receiver:
+                    async for item in receiver:
+                        print(item)
+
+                _logger.info(f"directory received from {Dock.peer_list.get_peer(event.handshake.peer_id)}")
+                transfers_book.add_to_completed(transfer_id, receiver)
         except Exception as e:
             print("*" * 80, e)
             traceback.print_exc()
 
     return handler
 
-
-#
-# class DirectorySender:
-#     def __init__(self, root_path, transfer_id, send_func):
-#         """
-#         Args:
-#             root_path(Path): root path to read from and start the transfer
-#             transfer_id(str): transfer id that synchronized both sides
-#             send_func(Callable[[bytes],Coroutine[None, None, None]): function to call upon to send file data when ready
-#         """
-#         self.state = TransferState.PREPARING
-#         self.root_path = root_path
-#         self.dir_iterator = self.root_path.rglob('*')
-#         self.current_file = None
-#         self.id = transfer_id
-#         self.send_func = send_func
-#
-#     async def start(self):
-#
-#         progress = tqdm.tqdm(
-#
-#         )
-#
-#         stack = [self.dir_iterator]
-#         while len(stack):
-#             for item in stack.pop():
-#                 file_items = []
-#                 empty_dirs = []
-#
-#                 if item.is_file():
-#                     file_item = FileItem(path=item, seeked=0)
-#                 elif item.is_dir():
-#                     if not any(item.iterdir()):
-#                         print("sending empty dir:", send_path(sock, item, self.root_path.parent))
-#                         continue
-#                     stack.append(item.iterdir())
-#
-#     def stop(self):
-#         pass
-#
-#     def pause(self):
-#         self.dir_iterator = itertools.chain([self.current_file], self.dir_iterator)
-#
-#     def continue_transfer(self):
-#         pass
-#
-#
-# class DirectoryReceiver:
-#     def __init__(self):
-#         self.state = TransferState.PREPARING
-#
-#     async def start(self):
-#         pass
-#
-#     def stop(self):
-#         pass
-#
-#     def continue_transfer(self):
-#         pass
-#
 
 @use.NotInUse
 class DirectoryTaskHandle(TaskHandle):
@@ -263,110 +211,3 @@ class DirectoryTaskHandle(TaskHandle):
 
     def chain(self):
         ...
-
-
-DOWNLOADS_DIR = "./down"
-CHUNK_SIZE = 1024
-
-
-def recv_something(_sock: socket.socket):
-    lenght = struct.unpack("!I", _sock.recv(4))[0]
-    data = _sock.recv(lenght)
-    return data.decode()
-
-
-def recv_file(_fpath: Path, _size: int, _sock: socket.socket):
-    with open(_fpath, "wb") as file:
-        while _size > 0:
-            data = _sock.recv(min(CHUNK_SIZE, _size))
-            if not data:
-                break
-            file.write(data)
-            _size -= len(data)
-
-
-def recv_dir(_sock: socket.socket):
-    while True:
-        raw_length = _sock.recv(4)
-        if not raw_length:
-            print("I am done")
-            return
-        con_len = struct.unpack("!I", raw_length)[0]
-        rel_path = _sock.recv(con_len).decode()
-        abs_path = Path(DOWNLOADS_DIR, rel_path)
-        if rel_path.endswith("/"):
-            # print("-" * 100)
-            os.makedirs(abs_path, exist_ok=True)
-            continue
-        # print("parent", abs_path.parent)
-        os.makedirs(abs_path.parent, exist_ok=True)
-        f_size = struct.unpack("!Q", _sock.recv(8))[0]
-        # print("got path", rel_path)
-        recv_file(abs_path, f_size, _sock)
-        # print("received file", f_size)
-
-
-def send_path(sock: socket.socket, path: Path, parent, end_with='/'):
-    path = path.relative_to(parent)
-    path_len = struct.pack('!I', len(str(path) + end_with))
-    sock.send(path_len)
-    sock.send((str(path.as_posix()) + end_with).encode())
-    return str(path.as_posix()) + end_with
-
-
-def send_file(sock: socket.socket, path: Path):
-    # progress = tqdm.tqdm(
-    #     range(path.stat().st_size),
-    #     desc=f"sending {path.relative_to(parent.parent)}",
-    #     unit='B',
-    #     unit_scale=True,
-    #     unit_divisor=1024
-    # )
-    s = send_path(sock, path, parent.parent, '')
-    sock.send(struct.pack('!Q', path.stat().st_size))
-    with path.open('rb') as f:
-        f_read = f.read
-        while True:
-            chunk = memoryview(f_read(chunk_size))
-            if not chunk:
-                break
-            sock.send(chunk)
-            # progress.update(len(chunk))
-    # progress.close()
-    return s
-
-
-def send_directory(sock: socket.socket, path: Path):
-    item_iter = path.iterdir()
-    stack = [item_iter, ]
-    while len(stack):
-        dir_iter = stack.pop()
-        for item in dir_iter:
-            if item.is_file():
-                send_file(sock, item)
-            elif item.is_dir():
-                if not any(item.iterdir()):
-                    print("sending empty dir:", send_path(sock, item, parent.parent))
-                    continue
-                stack.append(item.iterdir())
-
-
-def initiate():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("localhost", 8090))
-    print('connected')
-    with sock:
-        send_directory(sock, parent)
-    print("sent directory")
-
-
-if __name__ == "__main__":
-    # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # sock.bind(("localhost", 8090))
-    # sock.listen(1)
-    # ali_sock, ali_address = sock.accept()
-    # print("> connected..")
-    # recv_dir(ali_sock)
-    ...
-chunk_size = 1024
-parent = Path("C:\\Users\\7862s\\Desktop\\python\\VideosSampleCode")
