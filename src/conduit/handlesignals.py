693157a3cc210a88fb85794624ed7df60bb61e74
ShaikAli65 <alishaik.me@gmail.com>
2026-03-29 14:44:55 +0000

    chore!: using ui_events

--- src/conduit/handlesignals.py
+++ src/conduit/handlesignals.py
@@ -1,14 +1,19 @@
 import asyncio
 from typing import AsyncIterator
 
-from src.avails import DataWeaver, const
-from src.conduit import logger, webpage
+from src.avails import const
+from src.conduit import logger, ui_codec, webpage
 from src.conduit.handleprofiles import (
     align_profiles,
     set_selected_profile,
 )
-from src.conduit.headers import HANDLE
 from src.conduit.pagehandle import MessageFromFrontEndDispatcher
+from src.conduit.ui_codec import DataWeaver
+from src.conduit.ui_events import ConnectPeer, GossipSearchPeers, PeerConnectionStatus, PeerSummary, RequestPeerList, \
+    RequestProfilesSync, \
+    RequestUsersSync, \
+    SearchPeersByName, SearchResults, \
+    SetSelectedProfile, UsersSnapshot
 from src.core.peers import PeerService
 from src.managers import message
 
@@ -23,18 +28,18 @@ def register_handlers(
 
 ):
     dispatcher.register_handler(
-        HANDLE.CONNECT_USER, ConnectUserHandler(
+        ConnectPeer.event_name(), ConnectUserHandler(
             conn_service,
             msg_conn_service,
             peer_service,
             connectivity_checker,
         ))
-    dispatcher.register_handler(HANDLE.SYNC_USERS, SyncUsersHandler(peer_list))
-    dispatcher.register_handler(HANDLE.SEND_PROFILES, align_profiles)
-    dispatcher.register_handler(HANDLE.SET_PROFILE, set_selected_profile)
-    dispatcher.register_handler(HANDLE.SEARCH_FOR_NAME, SearchUserHandler(peer_service.kad_server))
-    dispatcher.register_handler(HANDLE.SEND_PEER_LIST, SendListHandler(peer_service))
-    dispatcher.register_handler(HANDLE.GOSSIP_SEARCH, GossipSearchHandler(peer_service))
+    dispatcher.register_handler(RequestUsersSync.event_name(), SyncUsersHandler(peer_list))
+    dispatcher.register_handler(RequestProfilesSync.event_name(), align_profiles)
+    dispatcher.register_handler(SetSelectedProfile.event_name(), set_selected_profile)
+    dispatcher.register_handler(SearchPeersByName.event_name(), SearchUserHandler(peer_service.kad_server))
+    dispatcher.register_handler(RequestPeerList.event_name(), SendListHandler(peer_service))
+    dispatcher.register_handler(GossipSearchPeers.event_name(), GossipSearchHandler(peer_service))
 
 
 def SearchUserHandler(peer_service: PeerService):
@@ -49,7 +54,7 @@ def SearchUserHandler(peer_service: PeerService):
             peer_service.search_for_peers_with_name(search_string),
             const.TIMEOUT_TO_GATHER_SEARCH_RESULTS
         )
-        await webpage.search_response(data.msg_id, peer_list, type="lists")
+        webpage.send_result(SearchResults(peer_list, "list", data.msg_id))
 
     return search_for_user
 
@@ -59,7 +64,7 @@ async def _response_gather_helper(iterator: AsyncIterator, timeout):
 
     async def gather():
         async for peer in iterator:
-            peer_list.append(peer)
+            peer_list.append(ui_codec.remote_peer_to_peer_summary(peer))
 
     t = asyncio.create_task(gather())
     try:
@@ -80,7 +85,9 @@ def GossipSearchHandler(peer_service: PeerService):
             peer_service.gossip_search(search_string),
             const.TIMEOUT_TO_GATHER_SEARCH_RESULTS
         )
-        await webpage.search_response(data.msg_id, peer_list, type="gossip")
+        webpage.send_result(
+            SearchResults(peer_list, "gossip", data.msg_id)
+        )
 
     return gossip_search
 
@@ -90,7 +97,12 @@ def SendListHandler(peer_service: PeerService):
         logger.debug("got a send list request")
         peer_list = await peer_service.get_more_peers()
         logger.debug(f"sending list {peer_list=}")
-        await webpage.search_response(data.msg_id, peer_list)
+        webpage.send_result(SearchResults(
+            [ui_codec.remote_peer_to_peer_summary(peer) for peer in peer_list]
+            if peer_list else [],
+            "list",
+            data.msg_id
+        ))
 
     return send_list
 
@@ -101,16 +113,15 @@ def ConnectUserHandler(
         peer_service,
         connectivity_checker,
 ):
-    async def connect_peer(handle_data: DataWeaver):
+    async def connect_peer(connect_peer_req: ConnectPeer):
         what = await message.connect_ahead(
-            handle_data.peer_id,
+            connect_peer_req.peer_id,
             conn_service,
             msg_conn_service,
             peer_service,
             connectivity_checker,
         )
-        await (webpage.peer_connected if what else webpage.failed_to_reach)(handle_data.peer_id)
-
+        webpage.notify(PeerConnectionStatus(connect_peer_req.peer_id, what))
     return connect_peer
 
 
@@ -119,9 +130,14 @@ def SyncUsersHandler(peer_list):
         refreshed = []
 
         for peer in peer_list.values():
-            if peer.is_online:
-                refreshed.append(peer)
+            refreshed.append(
+                PeerSummary(
+                    name=peer.username,
+                    ip=peer.ip,
+                    peer_id=peer.peer_id,
+                    online=peer.online,
+                ))
 
-        return await webpage.sync_users(refreshed)
+        webpage.send_result(UsersSnapshot(refreshed))
 
     return sync_users
