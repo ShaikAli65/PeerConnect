9c6e6574ec3f2f4533d046de51ef0f187120ccdf
ShaikAli65 <alishaik.me@gmail.com>
2026-05-01 17:35:04 +0000

    chore: remove obsolete test modules and mock utilities

--- tests/_path.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-from pathlib import Path
-
-if "tests" in Path(os.getcwd()).parts:
-    sys.path.append(str(Path(os.getcwd(),'..').resolve()))
-    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
--- tests/serverconn.py
+++
@@ -1,8 +0,0 @@
-import _path  # noqa
-from src.core import connectserver
-from src.managers.statemanager import State
-from tests.test import start_test
-
-if __name__ == "__main__":
-    s4 = State("connecting to servers",connectserver.initiate_connection)
-    start_test(s4)
