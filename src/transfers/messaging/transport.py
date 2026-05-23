a022ca54b001e980c1c4c2728a700e1668b8224f
ShaikAli65 <alishaik.me@gmail.com>
2026-05-23 22:52:36 +0000

    refactor!: remove `MsgSender` and `MsgReceiver`, introduce `MessageProtocol` and `MessageTransport` with improved messaging support
    - Replaced `MsgSender` and `MsgReceiver` with a streamlined `MessageProtocol` and `MessageTransport` architecture.
    - Added read receipts (`MSG_READ_RECEIPT`) alongside acknowledgments to enhance message tracking.
    - Refactored to modularize messaging logic under the `messaging` package for better clarity and maintainability.

---
+++ src/transfers/messaging/transport.py
@@ -0,0 +1,67 @@
+import asyncio
+import logging
+from contextlib import asynccontextmanager
+
+from avails import WireData
+from avails.exceptions import InvalidPacket
+from net import WireIO
+from src import net
+from src.transfers import HEADERS
+from .protocol import MessageProtocol
+
+logger = logging.getLogger(__package__)
+
+
+class MessageTransport:
+    def __init__(self, protocol: MessageProtocol):
+        self._msg_socket = net.MessageSocket(should_prune_buffer_on_full=False, logger=logger)
+        self.protocol = protocol
+        self._transport = None
+        self._receiver_task = None
+
+    def connection_made(self, connection: net.Connection):
+        self._msg_socket.update_transport(connection)
+        self._transport = connection
+
+    async def start_receiving(self):
+        while True:
+            # TODO: what if connection is lost? and it is established again? and we lost the update
+            # cause we didn't call wait_for before the connection was re-established
+            async with self._msg_socket.connection_restablished:
+                await self._msg_socket.connection_restablished.wait_for(lambda : self._transport is not None)
+
+            try:
+                await self._recv_loop()
+            except OSError as exc:
+                logger.info("!< transport closed")
+                await self.protocol.connection_lost(exc)
+
+    async def _recv_loop(self):
+        while True:
+            try:
+                wire_data = await WireIO.recv_msg(self._transport)
+                logger.debug(f"#< new msg {wire_data}")
+            except InvalidPacket:
+                logger.info(f"!< malformed packet", exc_info=True)
+                continue
+
+            if wire_data.match_header(HEADERS.MSG_READ_RECEIPT):
+                await self.protocol.message_receipt_received(
+                    wire_data.body["receipt"],
+                    wire_data.peer_id
+                )
+                continue
+
+            await self.protocol.message_received(wire_data.body, wire_data.peer_id)
+
+    async def send_data(self, data: WireData):
+        await self._msg_socket(bytes(data))
+
+    @asynccontextmanager
+    async def context_manager(self):
+        try:
+            self._receiver_task = asyncio.create_task(self.start_receiving())
+            async with self._msg_socket.context_manager():
+                yield self
+        finally:
+            self._receiver_task.cancel()
